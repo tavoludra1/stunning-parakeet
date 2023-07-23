931419fa6027b4b4c5996c35e67537c74a050e20
GustavoP <42284294+tavoludra1@users.noreply.github.com>
2023-07-23 19:24:54 +0000

    Cambio nombre index por server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500; // cambiar el puerto en producción
+
+
+
+
+// verificamos si estamos en producción o en desarrollo
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
