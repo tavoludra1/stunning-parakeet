3791783d9cdbeddcb6ed3c4d6c3f2a48fa26fe2c
GustavoP <42284294+tavoludra1@users.noreply.github.com>
2023-07-23 19:53:28 +0000

    codigo 404 en server

--- server.js
+++ server.js
@@ -10,6 +10,18 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 // adicionar control de rutas
 app.use('/', require('./routes/root'));
 
+// adicionar pagina de error 404 y mensajes error
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 No encontrado' });
+    } else {
+        res.type('txt').send('404 No encontrado');
+    }
+});
+
 
 
 
