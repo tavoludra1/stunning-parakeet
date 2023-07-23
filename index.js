fbddbf700182bda9af57c5294a7f4ce9f4c81c9c
GustavoP <42284294+tavoludra1@users.noreply.github.com>
2023-07-23 17:54:50 +0000

    hacer correcciones de los eventos

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const logEvents = require('./logEvents');
+const eventLog = require('./logEvents');
+const EventEmitter = require('events');
+
+class myEmitter extends EventEmitter {};
+
+// iniciar objeto
+const myEmitterInstance = new myEmitter(); 
+
+// adicionar listener
+myEmitterInstance.on('log', (message) => logEvents(message));
+
+setTimeout(() => {
+    myEmitterInstance.emit('log', 'evento emitido!');
+}, 2000);
